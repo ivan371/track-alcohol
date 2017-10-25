cee97bb138f427875b23c2d4aefd4ddfe47c85b6
kostet1994 <kostet1994@rambler.ru>
2017-10-25 14:32:17 +0300

    Почистил код и добавил разделитель между элементами

--- app/src/main/java/nagaiko/track_alcohol/fragments/RecyclerFragment.java
+++ app/src/main/java/nagaiko/track_alcohol/fragments/RecyclerFragment.java
@@ -1,17 +1,17 @@
 package nagaiko.track_alcohol.fragments;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.DividerItemDecoration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import nagaiko.track_alcohol.ClickRecyclerAdapter;
+import nagaiko.track_alcohol.recyclerview.ClickRecyclerAdapter;
 import nagaiko.track_alcohol.DataStorage;
 import nagaiko.track_alcohol.models.Cocktail;
 
@@ -29,11 +29,12 @@ public class RecyclerFragment extends Fragment implements
     private static final String VISIBLE_POSITION = "position";
 
     private static String[] names;
+    private static String[] ingredient;
     private DataStorage dataStorage = DataStorage.getInstance();
 
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (savedInstanceState != null) {
             Log.d(TAG, "savedInstanceState");
@@ -52,22 +53,27 @@ public class RecyclerFragment extends Fragment implements
 
     }
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         recyclerView = new RecyclerView(getActivity());
 
         Cocktail[] data = (Cocktail[])dataStorage.getData(DataStorage.COCKTAIL_FILTERED_LIST);
 
         names = new String[data.length];
+//        ingredient = new String[data.length];
         for (int i = 0; i < data.length; i++){
             names[i] = data[i].name;
+//            ingredient[i] = data[i].categoryName;
         }
 
         recyclerView.setAdapter(new ClickRecyclerAdapter(getActivity().getLayoutInflater(),names, this));
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         recyclerView.setHasFixedSize(true);
+
+        RecyclerView.ItemDecoration itemDecoration = new
+                DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(itemDecoration);
 
         return recyclerView;
     }
--- app/src/main/java/nagaiko/track_alcohol/recyclerview/ListRecyclerAdapter.java
+++ app/src/main/java/nagaiko/track_alcohol/recyclerview/ListRecyclerAdapter.java
@@ -1,10 +1,12 @@
-package nagaiko.track_alcohol;
+package nagaiko.track_alcohol.recyclerview;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import java.lang.ref.WeakReference;
+
+import nagaiko.track_alcohol.R;
 
 /**
  * Created by Konstantin on 24.10.2017.
@@ -14,10 +16,12 @@ public class ListRecyclerAdapter extends RecyclerView.Adapter<ListViewHolder> {
 
     private final WeakReference<LayoutInflater> mInflater;
     public final String[] names;
+//    public final String[] ingredient;
 
     public ListRecyclerAdapter(LayoutInflater inflater, String[] names) {
         mInflater = new WeakReference<LayoutInflater>(inflater);
         this.names = names;
+//        this.ingredient = ingredient;
     }
 
     @Override
@@ -34,6 +38,7 @@ public class ListRecyclerAdapter extends RecyclerView.Adapter<ListViewHolder> {
     @Override
     public void onBindViewHolder(ListViewHolder holder, int position) {
         holder.setName(names[position]);
+//        holder.setIngredient(ingredient[position]);
     }
 
     @Override
